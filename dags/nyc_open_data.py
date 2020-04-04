c9e60cf5ca2f5ebced81043d8f8898720edc00d0
Norton Li <nli@zipcodes-MBP.home>
2020-04-04 00:29:48 -0400

    3rd datasets added, done for open_nyc_data

--- dags/nyc_open_data.py
+++ dags/nyc_open_data.py
@@ -8,7 +8,7 @@ import csv
 import requests
 
 
-default_args = {
+default_args={
     'owner': 'Norton_Li',
     'start_date': datetime.now(),
     'retries': 5,
@@ -17,7 +17,7 @@ default_args = {
 
 
 dag = DAG(
-    dag_id = 'nyc_open_data',
+    dag_id='nyc_open_data',
     default_args=default_args,
     description='NYC public open data',
     )
@@ -29,12 +29,11 @@ def get_housing_data():
     """
     nyc_url = 'https://data.cityofnewyork.us/api/views/hg8x-zxpr/rows.csv?accessType=DOWNLOAD'
     response = requests.get(nyc_url)
-
-    with open('nyc_housing_data1.csv', 'wb') as file:
+    with open('nyc_housing_data.csv', 'wb') as file:
         file.write(response.content)
 
 
-t1a = PythonOperator(
+t1 = PythonOperator(
     task_id = 'get_nyc_housing_data',
     python_callable=get_housing_data,
     provide_context=False,
@@ -48,12 +47,11 @@ def get_shooting_data():
     """
     shooting_url = 'https://data.cityofnewyork.us/api/views/5ucz-vwe8/rows.csv?accessType=DOWNLOAD&api_foundry=true'
     response2 = requests.get(shooting_url)
-
     with open('nyc_shooting_data.csv', 'wb') as file2:
         file2.write(response2.content)
 
 
-t1b = PythonOperator(
+t2 = PythonOperator(
     task_id='get_nyc_shooting_data',
     python_callable=get_shooting_data,
     provide_context=False,
@@ -61,5 +59,21 @@ t1b = PythonOperator(
 )
 
 
+def get_nyc_hot_spot():
+    """
+    Free NYC Public hotspot dataset
+    """
+    hot_spot_url = 'https://data.cityofnewyork.us/api/views/varh-9tsp/rows.csv?accessType=DOWNLOAD'
+    response3 = requests.get(hot_spot_url)
+    with open('nyc_hot_spot.csv', 'wb') as file3:
+        file3.write(response3.content)
 
+
+t3 = PythonOperator(
+    task_id='get_nyc_hot_spot',
+    python_callable=get_nyc_hot_spot,
+    provide_context=False,
+    dag=dag,
 )
+
+t1 >> [t2, t3]
